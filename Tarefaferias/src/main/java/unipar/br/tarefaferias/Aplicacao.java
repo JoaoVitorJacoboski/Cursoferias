0f474fe79a1c6abb6e65cc9cdefe30c55042f751
Joao Vitor Jacoboski Liesenfeld <130612916+JoaoVitorJacoboski@users.noreply.github.com>
2025-02-02 23:16:11 -0300

    Add files via upload

---
+++ Tarefaferias/src/main/java/unipar/br/tarefaferias/Aplicacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unipar.br.tarefaferias;
+
+/**
+ *
+ * @author jjoao
+ */
+public class Tarefaferias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
